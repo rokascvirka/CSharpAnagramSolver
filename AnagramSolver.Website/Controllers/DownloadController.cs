58392744b9b7098140b3b6550392b5550fba0c8a
Rokas Cvirka <rokas42@gmail.com>
2022-06-14 23:51:25 +0300

    Added Download File option

---
+++ AnagramSolver.Website/Controllers/DownloadController.cs
@@ -0,0 +1,23 @@
+﻿using AnagramSolverWebsite.Controllers;
+using BuisnessLogic;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace AnagramSolver.Website.Controllers
+{
+    [Route("{controller}/{action}/{filename}")]
+    public class DownloadController : Controller
+    {
+        public FileResult DownloadFile(string filename)
+        {
+            //Build the File Path.
+            string path = "C:\\Users\\rokas.cvirka\\Documents\\" + filename + ".txt";
+
+            //Read the File data into Byte Array.
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            //Send the File to Download.
+            return File(bytes, "application/octet-stream", filename + ".txt");
+        }
+    }
+}
